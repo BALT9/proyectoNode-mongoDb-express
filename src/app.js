9e791a6f33f84f2eecbb45aaf4318ac43aa56ec0
Willy Balt <willybalt.2017@gmail.com>
2024-12-13 11:44:10 -0400

    guarda los datos en mongodb y estructura los datos

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 
+app.use(express.json())
+
 app.use("/api",authRoutes);
+
 
 
 export default app;
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,28 @@
-
+// importa el modelo de user
+import User from '../models/user.model.js'
 // exporta 2 controladores
-export const register = (req,res)=>res.send('register');
+// req obtiene y res envia 
+export const register = async (req,res)=>{
+    const {email, password, username} = req.body
+    // console.log(req.body)
+    
+    try {
+        const newUser =  new User({
+            username,
+            email,
+            password
+        })
+    
+        // muestra los datos en consola con el modelo user
+        console.log("se guardo en mongoDB")
+        console.log(newUser)
+        // guarda en la base de datos
+        const userSave = await newUser.save()
+        // respuesta al frontend
+        res.send('registrando y guardado: '+userSave)
+
+    } catch (error){
+        console.log(error)
+    }
+};
 export const login = (req,res)=>res.send('login');
